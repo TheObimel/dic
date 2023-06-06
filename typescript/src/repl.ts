acfb7abdcc98fb1cdd1caf6465a3fd35119d37d4
theobimel <theobimel@gmail.com>
2023-06-06 23:45:14 +0100

    1.5 ts repl start

---
+++ typescript/src/repl.ts
@@ -0,0 +1,20 @@
+import { stdin as input, stdout as output } from 'node:process';
+import * as readline from 'node:readline/promises';
+import { Lexer } from "./lexer";
+import { Token, TokenTypes } from "./token";
+
+async function start(){
+    const rl = readline.createInterface({ input, output });
+    rl.on('line', (line) => {
+        const lexer = new Lexer(line);
+        let token: Token | undefined;
+
+        while(token?.type !== TokenTypes.EOF) {
+            token = lexer.nextToken();
+            console.log(token);
+        }
+    });
+
+}
+
+start();
